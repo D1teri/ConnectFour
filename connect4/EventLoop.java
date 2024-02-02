e158e96b13b1078695fafc488617808ef7ba4ddf
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2024-02-02 11:40:07 -0800

    bases

--- connect4/EventLoop.java
+++ connect4/EventLoop.java
@@ -8,6 +8,12 @@ package connect4;
 
 public class EventLoop
 {   
+    public static void main(String[] args){
+        EventLoop eventLoop = new EventLoop();
+        eventLoop.run();
+    }
     
-    
+    public void run(){
+        
+    }
 }
